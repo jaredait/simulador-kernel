62a94cd0562984bc6314a18332f8cdb4a98162a1
jaredait <jaredampudia@gmail.com>
2021-09-24 13:51:15 -0500

    agregar funcionamiento puntos 1 y 2

---
+++ simulador-kernel-SO1/BCP.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simulador_kernel_SO1
+{
+    class BCP
+    {
+        private String Nombre;
+        private int Prioridad;
+        private int TiempoEjecucion;
+        private int PID;
+        private String Estado;
+        private DateTime HoraCreacion;
+        private TimeSpan TiempoRestante;
+
+        public BCP(string nombre, int prioridad, int tiempoEjecucion)
+        {
+            this.Nombre = nombre;
+            this.Prioridad = prioridad;
+            this.TiempoEjecucion = tiempoEjecucion;
+        }
+
+        public string nombre { get => Nombre; set => Nombre = value; }
+        public int prioridad { get => Prioridad; set => Prioridad = value; }
+        public int tiempoEjecucion { get => TiempoEjecucion; set => TiempoEjecucion = value; }
+        public int pid { get => PID; set => PID = value; }
+        public string estado { get => Estado; set => Estado = value; }
+        public DateTime horaCreacion { get => HoraCreacion; set => HoraCreacion = value; }
+        public TimeSpan tiempoRestante { get => TiempoRestante; set => TiempoRestante = value; }
+
+
+        public int CompareTo(BCP bcpComparar)
+        {
+            // A null value means that this object is greater.
+            if (bcpComparar == null)
+                return 1;
+
+            else
+                return this.prioridad.CompareTo(bcpComparar.prioridad);
+        }
+
+        public override string ToString()
+        {
+            return "________________________________" +
+                "\nNombre: " + this.nombre +
+                "\nPrioridad: " + this.prioridad +
+                "\nTiempo de ejecución: " + tiempoEjecucion + 
+                "\nPID: " + pid +
+                "\nEstado: " + estado + 
+                "\nHora de creación: " + horaCreacion + 
+                "\nTiempo Restante: " + tiempoRestante;
+        }
+    }
+}
--- simulador-kernel-SO1/Program.cs
+++ simulador-kernel-SO1/Program.cs
@@ -1,12 +1,125 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace simulador_kernel_SO1
 {
     class Program
     {
+        private static List<BCP> TodosProcesos = new List<BCP>();
+        private static List<BCP> Preparados = new List<BCP>(); 
+        private List<BCP> Suspendidos = new List<BCP>();
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            DateTime date1 = new DateTime(2010, 1, 1, 8, 0, 0);
+            DateTime date2 = new DateTime(2010, 1, 1, 8, 0, 15);
+
+            // Calculate the interval between the two dates.
+            TimeSpan interval = date2 - date1;
+            Console.WriteLine("{0} - {1} = {2}", date2, date1, interval.TotalSeconds);
+            */
+
+
+            Program program = new Program();
+            int opcionMenu = 0;
+            bool continuar = true;
+
+
+
+
+
+            do
+            {
+                Console.Clear();
+                program.ImprimirMenu();
+                opcionMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcionMenu)
+                {
+                    case 1:
+                        program.CrearProceso();
+                        break;
+
+                    case 2:
+                        program.EjecutarProceso();
+                        break;
+
+                    default:
+                        break;
+                }
+
+                continuar = opcionMenu == 8 ? false : true;
+            } while (continuar);
+
+  
+        }
+
+        private void ImprimirMenu()
+        {
+            Console.WriteLine("1. Crear Proceso");
+            Console.WriteLine("2. Ejecutar Procesos");
+            Console.WriteLine("3. Reanudar Proceso");
+            Console.WriteLine("4. Matar Proceso");
+            Console.WriteLine("5. Ver todos los procesos");
+            Console.WriteLine("6. Ver procesos preparados");
+            Console.WriteLine("7. Ver procesos suspendidos");
+            Console.WriteLine("8. Salir");
+            Console.WriteLine("______________________________");
+        }
+
+        //  1
+        private void CrearProceso()
+        {
+            string nombreProceso;
+            int prioridad;
+            int tiempoEjecucion;
+
+            Console.WriteLine("Nombre del proceso:");
+            nombreProceso = Console.ReadLine();
+            Console.WriteLine("Prioridad: (máxima prioridad es 0)");
+            prioridad = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Tiempo de ejecución: (en segundos)");
+            tiempoEjecucion = Convert.ToInt32(Console.ReadLine());
+
+            BCP bcp = new BCP(nombreProceso, prioridad, tiempoEjecucion);
+
+            // asignar parametros al BCP
+            bcp.pid = TodosProcesos.Count;
+            bcp.estado = "P";
+            bcp.horaCreacion = DateTime.Now;
+
+            TodosProcesos.Add(bcp);
+            Preparados.Add(bcp);
+
+            // ordenar la lista Preparados segun su prioridad en orden ascendente
+            Preparados.Sort(delegate (BCP x, BCP y)
+            {
+                return x.prioridad.CompareTo(y.prioridad);
+            });
+        }
+
+        private void EjecutarProceso()
+        {
+            BCP bcp = Preparados[0];
+            Preparados.RemoveAt(0);
+            bcp.tiempoRestante = TimeSpan.FromTicks(bcp.tiempoEjecucion * 10000000);
+
+            Console.WriteLine("\nEjecutando:\n{0}\n", bcp.ToString());
+
+            int a = bcp.tiempoEjecucion;
+            while (a >= 0)
+            {
+                Console.Write("\rTiempo restante {0:00}", a);
+                System.Threading.Thread.Sleep(1000);
+                bcp.tiempoRestante = TimeSpan.FromTicks(a * 10000000);
+                a--;
+            }
+
+            Console.WriteLine("\n\n\nPROCESO TERMINADO\n{0}\n", bcp.ToString());
+
+            Console.ReadKey();
         }
     }
 }
