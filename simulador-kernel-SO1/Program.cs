1fc42de58d2d59023d038a54e10f5a5cc3d4c976
jaredait <jaredampudia@gmail.com>
2021-09-24 12:03:40 -0500

    agregar proyecto

---
+++ simulador-kernel-SO1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simulador_kernel_SO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
